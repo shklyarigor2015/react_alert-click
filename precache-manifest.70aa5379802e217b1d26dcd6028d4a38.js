8601f76524ada0874dae0b5925522688b98dd5c5
unknown <shklyarigor2015@gmail.com>
2019-09-13 01:50:37 +0300

    solution

---
+++ precache-manifest.70aa5379802e217b1d26dcd6028d4a38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87fba7202039fba3e4e18a70c58a9c2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8eaf254f4ba9528936bd",
+    "url": "./static/css/main.d9b57ff3.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "8eaf254f4ba9528936bd",
+    "url": "./static/js/main.a5212816.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
