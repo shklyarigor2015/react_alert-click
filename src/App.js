93cc1e47436549a444bb6477198a65f3353d0a4a
unknown <shklyarigor2015@gmail.com>
2019-09-13 01:43:19 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
+const onClickReaction = massage => alert('Button was clicked!');
+
 function App() {
   return (
-    <div>
-
+    <div className="main">
+      <button
+        onClick={() => onClickReaction()}
+        type="button"
+        className="btn"
+      >
+        Click me
+      </button>
     </div>
   );
 }
